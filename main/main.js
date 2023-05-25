99726c6bf1ae02b0f90edbd9af298ff2f545411d
Bhuwan <bhubanpaudel79@gmail.com>
2023-05-25 09:26:46 +0545

    made log in form with register . I modifiy the exsiting project just  learn js .It is not complete I try to make complete

---
+++ main/main.js
@@ -0,0 +1,27 @@
+const labels = document.querySelectorAll('.form-control label')
+
+const email = document.getElementById('email');
+const btn = document.getElementById('btn');
+ const password = document.getElementById('password');
+
+ const form = document.getElementById('form')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
+})
+
+
+btn.addEventListener('click',(e) =>{
+    if(email.value=== '' || password.value === ''){
+        alert('please fill all ')
+        e.preventDefault();
+    }else if(email.value === 'bhubanpaudel79@gmail.com' && password.value === '12345'){
+    
+      window.location.href = "/register/register.html";
+    }else{
+        alert('you entered the wrong email or password')
+    }
+})
