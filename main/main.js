59d8c41f758cebc9ba2a24b2c8c696f65bc9d838
Bhuwan <bhubanpaudel79@gmail.com>
2023-05-28 16:43:20 +0545

    I tried everything but still the problem is not working  I quit "

--- main/main.js
+++ main/main.js
@@ -1,5 +1,9 @@
-const labels = document.querySelectorAll('.form-control label')
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Your code here
+    const labels = document.querySelectorAll('.form-control label')
+    
 const email = document.getElementById('email');
 const btn = document.getElementById('btn');
  const password = document.getElementById('password');
@@ -13,17 +17,18 @@ labels.forEach(label => {
         .join('')
 })
 
-
-btn.addEventListener('click',(e) =>{
-    if(email.value=== '' || password.value === ''){
-        alert('please fill all ')
-        e.preventDefault();
-    }else if(email.value === 'bhbanpaudel79@gmail.com' && password.value === '12345'){
-    
-
-      window.location.href = "/log/log.html";
-
-    }else{
-        alert('you entered the wrong email or password')
+btn.addEventListener('click', (e) => {
+    console.log('button clicked')
+    if (email.value === '' || password.value === '') {
+       
+        alert('Please fill in all fields ');
+    } else if (email.value === 'bhubanpaudel79@gmail.com' && password.value === '12345') {
+        // alert('Right');
+        console.log('you came')
+        window.location.href = "/log/log.html";
+    } else {
+        alert('You entered the wrong email or password');
     }
-})
+});
+
+});
